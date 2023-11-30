48c6f699f0b2734e35dc00e2cfd9b33752c38b1d
Martin Demko <325073@mail.muni.cz>
2023-11-30 11:55:11 +0100

    solves bug with missing empty file in average_plot_profile

--- wrappers/plot_average_peak_profile/script.py
+++ wrappers/plot_average_peak_profile/script.py
@@ -7,8 +7,8 @@ import math
 import subprocess
 import re
 from snakemake.shell import shell
-#import pdfkit
-from fpdf import FPDF
+import pdfkit
+#from fpdf import FPDF
 
 f = open(snakemake.log.run, 'a+')
 f.write("\n##\n## RULE: plot_average_peak_profile \n##\n")
@@ -25,25 +25,34 @@ if os.path.isfile(snakemake.input.bed) and os.path.getsize(snakemake.input.bed) 
     f = open(snakemake.log.run, 'at')
     f.write("## WARNING: Input file "+snakemake.input.bed+" is missing or empty\n")
     f.close()
-    #pdfkit.from_string('Input file '+snakemake.input.bed+' is missing or empty!', snakemake.output.plot)
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("helvetica", size=16)
-    pdf.write(txt='Input file '+snakemake.input.bed+' is missing or empty!')
-    pdf.output(snakemake.output.plot)
-    
+
+    pdfkit.from_string('Input file '+snakemake.input.bed+' is missing or empty!', snakemake.output.plot)
+    #pdf = FPDF()
+    #pdf.add_page()
+    #pdf.set_font("helvetica", size=16)
+    #pdf.write(txt='Input file '+snakemake.input.bed+' is missing or empty!')
+    #pdf.output(snakemake.output.plot)
+
+    pdfkit.from_string('Input file '+snakemake.input.bed+' is missing or empty!', snakemake.output.profile)
+
+    command = "touch "+snakemake.output.profile_data+" >> "+snakemake.log.run+" 2>&1"
+    f = open(snakemake.log.run, 'at')
+    f.write("## COMMAND: "+command+"\n")
+    f.close()
+    shell(command)
+
 else:
     extra = "--sortRegions keep"
     if "skip_zeros" in snakemake.params and snakemake.params.skip_zeros:
         extra = extra+" --skipZeros"
     title = snakemake.params.title+" ("+snakemake.wildcards.filt+")"
-    
+
     command = "computeMatrix reference-point -S "+snakemake.input.bwg+" -R "+snakemake.input.bed+" -o "+snakemake.params.mtx+" "+extra+" --samplesLabel "+snakemake.params.sample_name+" -a "+str(snakemake.params.after)+" -b "+str(snakemake.params.before)+" -p "+str(snakemake.threads)+" >> "+snakemake.log.run+" 2>&1"
     f = open(snakemake.log.run, 'at')
     f.write("## COMMAND: "+command+"\n")
     f.close()
     shell(command)
-    
+
     # ## PLOT ONLY HEATMAP
     # # plotHeatmap -m ${i} -o ${i%.mtx.gz}.heatmap.pdf --plotTitle "$(basename $i)" --dpi 300 --heatmapWidth 22 --startLabel start --endLabel end --whatToShow "heatmap and colorbar"
     # command = "plotHeatmap -m "+snakemake.params.data+" -o "+snakemake.output.heatmap+" --regionsLabel peaks --plotTitle "+title+" --dpi "+str(snakemake.params.dpi)+" --heatmapWidth 22 --refPointLabel peak_summit --whatToShow \"heatmap and colorbar\" >> "+snakemake.log.run+" 2>&1"
@@ -70,7 +79,7 @@ else:
     f.write("## COMMAND: "+command+"\n")
     f.close()
     shell(command)
-    
+
     ## PLOT HEATMAP AND PROFILE
     # plotHeatmap -m ${i} -o ${i%.mtx.gz}.combined.pdf --plotTitle "$(basename $i)" --dpi 300 --averageType mean --plotType std --heatmapWidth 12 --startLabel start --endLabel end
     command = "plotHeatmap -m "+snakemake.params.mtx+\
@@ -88,7 +97,7 @@ else:
     f.write("## COMMAND: "+command+"\n")
     f.close()
     shell(command)
-    
+
     command = "rm -f "+snakemake.params.mtx+" >> "+snakemake.log.run+" 2>&1"
     f = open(snakemake.log.run, 'at')
     f.write("## COMMAND: "+command+"\n")
